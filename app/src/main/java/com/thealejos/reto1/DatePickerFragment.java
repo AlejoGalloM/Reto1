8440fb867ed0d47fcc189c9dc9cdfb7808c549b6
AlejoGalloM <48385049+AlejoGalloM@users.noreply.github.com>
2020-08-24 19:13:11 -0500

    Cambios

---
+++ app/src/main/java/com/thealejos/reto1/DatePickerFragment.java
@@ -0,0 +1,38 @@
+package com.thealejos.reto1;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.widget.DatePicker;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+
+public class DatePickerFragment extends DialogFragment {
+
+    private DatePickerDialog.OnDateSetListener listener;
+
+    public static DatePickerFragment newInstance(DatePickerDialog.OnDateSetListener listener){
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setListener(listener);
+        return fragment;
+    }
+
+    public void setListener(DatePickerDialog.OnDateSetListener listener){
+        this.listener =listener;
+    }
+
+    @NonNull
+    public Dialog onCreateDialog(Bundle saveInstanceState) {
+        final Calendar calendar = Calendar.getInstance();
+        int anio = calendar.get(Calendar.YEAR);
+        int mes = calendar.get(Calendar.MONTH);
+        int dia = calendar.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(getActivity(), (DatePickerDialog.OnDateSetListener) this, anio, mes, dia);
+    }
+
+}
+
--- app/src/main/java/com/thealejos/reto1/Edad.java
+++ app/src/main/java/com/thealejos/reto1/Edad.java
@@ -2,13 +2,43 @@ package com.thealejos.reto1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Edad extends AppCompatActivity {
+
+
+    EditText editTextCalendaro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edad);
+        editTextCalendaro = (EditText) findViewById(R.id.calendario);
+        editTextCalendaro.setOnClickListener(view -> showDatePickerDialog());
+
     }
+
+    private void showDatePickerDialog() {
+        DatePickerFragment newFragment = DatePickerFragment.newInstance(new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                final String selectedDate = day + " / " + (month + 1) + " / " + year;
+                editTextCalendaro.setText(selectedDate);
+            }
+        });
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
 }
+
